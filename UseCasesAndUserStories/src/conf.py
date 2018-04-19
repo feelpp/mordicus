f36cf03e511972f18e53e2437e8d20648be96955
Basile MARCHAND <marchand@bmarchand.materiaux.ensmp.fr>
2018-04-19 18:07:18 +0200

    add a test to disable the plantuml extension if the needed module is not installed

--- UseCasesAndUserStories/src/conf.py
+++ UseCasesAndUserStories/src/conf.py
@@ -249,6 +249,11 @@ intersphinx_mapping = {'http://docs.python.org/': None}
 
 #import os
 #print( os.getcwd() )
+try:
+    import sphinxcontrib.plantuml
+    extensions = ['sphinxcontrib.plantuml', ]
+    plantuml = 'java -jar ../bin/plantuml.jar'
+except ImportError:
+    print("The plantuml extension is disabled --- if you want to use it install")
+    print("      sphinxcontrib.plantuml")
 
-extensions = ['sphinxcontrib.plantuml', ]
-plantuml = 'java -jar ../bin/plantuml.jar'
