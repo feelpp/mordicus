e48f5ef6a818ef9f3acc8f2ea609d20451202a7d
Fabien Casenave <fabien.casenave@safrangroup.com>
2020-02-21 15:59:56 +0100

    correct bug in dualSolutionComponents in src/poc-1/src/Mordicus/Modules/Safran/IO/ZsetSolutionReader.py

--- src/poc-1/src/Mordicus/Modules/Safran/IO/ZsetSolutionReader.py
+++ src/poc-1/src/Mordicus/Modules/Safran/IO/ZsetSolutionReader.py
@@ -9,16 +9,16 @@ import os
 
 primalSolutionComponents = {1:[""], 2:["1", "2"], 3:["1", "2", "3"]}
 #niROM solution:
-#dualSolutionComponents = {1:["11"], 3:["11", "22", "12"], 6:["11", "22", "33", "12", "23", "31"]}
-dualSolutionComponents = {1:["11"], 3:["11", "22", "12"], 6:["11", "22", "33", "12", "31", "23"]}
+#dualSolutionComponents = {1:[""], 3:["11", "22", "12"], 6:["11", "22", "33", "12", "23", "31"]}
+dualSolutionComponents = {1:[""], 3:["11", "22", "12"], 6:["11", "22", "33", "12", "31", "23"]}
 
 
 def ReadSnapshotComponent(solutionFileName, fieldName, time, primality=True):
     """
     Functional API
-    
+
     Reads a snapshots from the Z-set solution file "solutionFileName" (.ut), at time "time" and of primality "primality", from the HF computation
-            
+
     Parameters
     ----------
     solutionFileName : str
@@ -29,7 +29,7 @@ def ReadSnapshotComponent(solutionFileName, fieldName, time, primality=True):
         time at which the snapshot is read
     primality : bool
         primality of the solution from which the snapshot is read
-                    
+
     Returns
     -------
     np.ndarray
@@ -42,12 +42,12 @@ def ReadSnapshotComponent(solutionFileName, fieldName, time, primality=True):
 def ReadTimeSequenceFromSolutionFile(solutionFileName):
     """
     Reads the time sequence from the Z-set solution file "solutionFileName" (.ut) (may be different from the ones defined in the input data file if the solver chose to solve at additional time steps)
-            
+
     Parameters
     ----------
     solutionFileName : str
-        Z-set solution file 
-    
+        Z-set solution file
+
     Returns
     -------
     np.ndarray
@@ -76,16 +76,16 @@ class ZsetSolutionReader(SolutionReaderBase):
         super(ZsetSolutionReader, self).__init__()
 
         assert isinstance(solutionFileName, str)
-        
+
         folder = str(Path(solutionFileName).parents[0])
         suffix = str(Path(solutionFileName).suffix)
         stem = str(Path(solutionFileName).stem)
-        
-        if MPI.COMM_WORLD.Get_size() > 1: # pragma: no cover 
+
+        if MPI.COMM_WORLD.Get_size() > 1: # pragma: no cover
             self.solutionFileName = folder + os.sep + stem + "-" + str(MPI.COMM_WORLD.Get_rank()+1).zfill(3) + suffix
         else:
-            self.solutionFileName = solutionFileName        
-        
+            self.solutionFileName = solutionFileName
+
 
     def ReadSnapshotComponent(self, fieldName, time, primality=True):
         from BasicTools.IO import UtReader as UR
@@ -100,7 +100,7 @@ class ZsetSolutionReader(SolutionReaderBase):
             time,
             atIntegrationPoints=atIntegrationPoints,
         )
-    
+
 
     def ReadSnapshot(self, fieldName, time, numberOfComponents, primality=True):
         from BasicTools.IO import UtReader as UR
@@ -124,16 +124,16 @@ class ZsetSolutionReader(SolutionReaderBase):
             atIntegrationPoints=atIntegrationPoints))
 
         return np.concatenate(res)
-    
-    
+
+
     """def ReadSnapshotTimeSequenceAndAddToSolution(self, solution, timeSequence, fieldName):
-        
+
         primality = solution.GetPrimality()
         numberOfComponents = solution.GetNbeOfComponents()
-        
+
         for t in timeSequence:
             solution.AddSnapshot(self.ReadSnapshot(fieldName, t, numberOfComponents, primality), t)"""
-            
+
 
     def ReadTimeSequenceFromSolutionFile(self):
         from BasicTools.IO import UtReader as UR
