ca43077b2bfcbec576950743885207092457cd25
grosjean1 <elise.grosjean2@gmail.com>
2020-07-07 16:56:15 +0200

    GMSH reader v2

--- src/poc-1/src/Mordicus/Modules/sorbonne/IO/GmshMeshReader.py
+++ src/poc-1/src/Mordicus/Modules/sorbonne/IO/GmshMeshReader.py
@@ -5,7 +5,7 @@ from mpi4py import MPI
 from pathlib import Path
 import os
 import meshio
-
+import argparse
 
 
 def ReadMesh(meshFileName):
@@ -81,3 +81,141 @@ class GmshMeshReader(MeshReaderBase):
 
         return mesh
  
+
+def CheckAndConvertMeshFFtoGMSH(meshFileName,meshFileNameGMSH):
+     """
+    Functional API
+    
+    Convert the mesh from FreeFem++ to Gmsh mesh format "meshFileName" (.msh)
+            
+    Parameters
+    ----------
+    meshFileName : str
+        Gmsh or FF++ mesh file 
+                    
+    Returns
+    -------
+    GMSH mesh
+        mesh of the HF computation
+    """
+
+     print("meshfilename: ",meshFileName)
+     meshFile=open(meshFileName,"r")
+     firstline=meshFile.readline()
+     firstlinebis="MeshFormat"
+
+     if firstline[1:-1] == firstlinebis[:]: #si c'est deja au format GMSH 
+         print("GMSH format")
+         os.rename(meshFileName, meshFileNameGMSH)
+     else: #si c'est format FF++, on convertir au format GMSH
+         print("Convert FF++ format to GMSH...")
+     
+         ConvertFFtoGMSH2D(meshFileName,meshFileNameGMSH)
+         print("Converted!")
+      
+
+
+    
+def ConvertFFtoGMSH2D(meshFileName,meshFileNameGMSH):
+   
+    # Read file and store data
+    infile = open(meshFileName, 'r') 
+    outfile = open(meshFileNameGMSH, 'w') 
+    Lines = infile.readlines()
+
+    # first line is "NNodes NTri Nseg"
+    data = Lines[0].strip('\n').split(" ")
+    nnodes = int(data[0])
+    nelem = []
+    nelem_tot = 0
+    for d in data[1:len(data)]:
+        nelem.append(int(d))
+        nelem_tot += int(d)
+    print("Nnodes = " + str(nnodes) + "\nNelem  = "+str(nelem_tot))
+
+    # Header
+    outfile.write("$MeshFormat\n2.2 0 8\n$EndMeshFormat\n$Nodes\n");
+    # Points
+    cpt = 1 #line counter
+    outfile.write(str(nnodes)+"\n");
+    for i in range(nnodes): 
+        outfile.write(str(i+1)+ " " + Lines[cpt][:-2]+" "+str(0)+"\n");
+        cpt += 1
+    outfile.write("$EndNodes\n");
+    # Element
+    outfile.write("$Elements\n");
+    outfile.write(str(nelem_tot)+"\n");
+    for i in range(nelem_tot):
+    #Loop on all element, without knowing what type they are
+        elem = Lines[cpt].strip('\n').split(" ")
+        if(len(elem)==3):
+      # Segment
+            outfile.write(str(i+1)+ " " ) # new element
+            outfile.write("1 2 " + str(elem[2])+ " 0 " + str(elem[0]) +" "+str(elem[1])+"\n")
+        elif(len(elem)==4):
+            #Triangle
+            outfile.write(str(i+1)+ " " ) # new element
+            outfile.write("2 2 " + str(elem[3])+ " 0 " + str(elem[0]) +" "+str(elem[1])+" "+str(elem[2])+"\n")
+        else:
+            #Do not know
+            print("unrecognized format! ")
+        cpt += 1
+    outfile.write("$EndElements\n");
+    # close everything
+    infile.close()
+    outfile.close()
+
+
+
+
+def ConvertFFtoGMSH3D(meshFileName,meshFileNameGMSH):
+   
+    # Read file and store data
+    infile = open(meshFileName, 'r') 
+    outfile = open(meshFileNameGMSH, 'w') 
+    Lines = infile.readlines()
+
+    # first line is "NNodes NTri Nseg"
+    data = Lines[0].strip('\n').split(" ")
+    nnodes = int(data[0])
+    nelem = []
+    nelem_tot = 0
+    for d in data[1:len(data)]:
+        nelem.append(int(d))
+        nelem_tot += int(d)
+    print("Nnodes = " + str(nnodes) + "\nNelem  = "+str(nelem_tot))
+
+    # Header
+    outfile.write("$MeshFormat\n2.2 0 8\n$EndMeshFormat\n$Nodes\n");
+    # Points
+    cpt = 1 #line counter
+    outfile.write(str(nnodes)+"\n");
+    for i in range(nnodes): 
+        outfile.write(str(i+1)+ " " + Lines[cpt]);
+        cpt += 1
+    outfile.write("$EndNodes\n");
+    # Element
+    outfile.write("$Elements\n");
+    outfile.write(str(nelem_tot)+"\n");
+    for i in range(nelem_tot):
+    #Loop on all element, without knowing what type they are
+        elem = Lines[cpt].strip('\n').split(" ")
+        if(len(elem)==3):
+      # Segment
+            outfile.write(str(i+1)+ " " ) # new element
+            outfile.write("1 2 " + str(elem[2])+ " 0 " + str(elem[0]) +" "+str(elem[1])+"\n")
+        elif(len(elem)==4):
+            #Triangle
+            outfile.write(str(i+1)+ " " ) # new element
+            outfile.write("2 2 " + str(elem[3])+ " 0 " + str(elem[0]) +" "+str(elem[1])+" "+str(elem[2])+"\n")
+        else:
+            #Do not know
+            print("unrecognized format! ")
+        cpt += 1
+    outfile.write("$EndElements\n");
+    # close everything
+    infile.close()
+    outfile.close()
+
+
+
