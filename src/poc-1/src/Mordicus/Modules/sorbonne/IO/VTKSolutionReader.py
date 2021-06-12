770427258da02d1f74a927c157d7e21ceff777f3
Elise Grosjean <elise.grosjean@upmc.fr>
2021-06-12 10:56:17 +0200

    cell data to point data vtk reader

--- src/poc-1/src/Mordicus/Modules/sorbonne/IO/VTKSolutionReader.py
+++ src/poc-1/src/Mordicus/Modules/sorbonne/IO/VTKSolutionReader.py
@@ -5,7 +5,8 @@ from Mordicus.Core.IO.SolutionReaderBase import SolutionReaderBase
 from mpi4py import MPI
 from pathlib import Path
 import os
-
+import vtk
+from vtk.util.numpy_support import vtk_to_numpy
 
 class VTKSolutionReader(SolutionReaderBase):
     """
@@ -34,5 +35,20 @@ class VTKSolutionReader(SolutionReaderBase):
         #print(data)
         npArray = dsa.WrapDataObject(data).GetPointData().GetArray(self.SolutionName)
       
+        return npArray
+
+    def VTKReadToNpCellToPointData(self,FileName): #.vtu
+        
+        reader = vtk.vtkXMLUnstructuredGridReader() 
+        reader.SetFileName(FileName)
+        reader.Update()
+
+        converter = vtk.vtkCellDataToPointData() #interpolation CellData To PointData
+        converter.ProcessAllArraysOn()
+        converter.SetInputConnection(reader.GetOutputPort())
+        converter.Update()
+        npArray = converter.GetOutput().GetPointData().GetArray(self.SolutionName)
+        npArray=vtk_to_numpy(npArray)
+
         return npArray
 
