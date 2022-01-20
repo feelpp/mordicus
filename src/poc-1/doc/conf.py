fe1206988b7d3cf5e298001e1c746ca11b849ae9
Fabien Casenave <fabien.casenave@safrangroup.com>
2022-01-20 16:34:47 +0100

    configure autodoc with readthedocs theme, and set 75% page width for html doc

--- src/poc-1/doc/conf.py
+++ src/poc-1/doc/conf.py
@@ -95,7 +95,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -106,7 +107,8 @@ html_theme = 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+html_css_files = ['custom.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
